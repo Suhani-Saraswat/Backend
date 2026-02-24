a33401c5452a8ad359c7fb1e960654acdaf560e7
Suhani Saraswat <suhanisaraswat1795@gmail.com>
2026-02-24 20:54:20 +0530

    Add Request func

--- ExpressDir/index.js
+++ ExpressDir/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 
-console.log(app);
-
 let port = 3000;
 
-app.listen(port , () =>{
+app.listen(port , () => {
     console.log(`App is listening on port ${port}`);
+});
+
+app.use((req, res) => {
+    console.log("request received");
 });
