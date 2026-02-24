9115acbaa94c7d0c98c6d600613fc2c56cc29c1e
Suhani Saraswat <suhanisaraswat1795@gmail.com>
2026-02-24 22:29:39 +0530

    query String

--- ExpressDir/index.js
+++ ExpressDir/index.js
@@ -11,21 +11,36 @@ app.get("/", (req, res) => {
     res.send("you contacted root path");
 });
 
-app.get("/apple", (req, res) => {
-    res.send("you contacted apple path");
+app.get("/:username/:id", (req, res) => {
+    let { username, id } = req.params;
+    let htmlStr = `<h1>Welcome to the page of @${username}!</h1>`
+    res.send(htmlStr);
 });
 
-app.get("/orange", (req, res) => {
-    res.send("you contacted orange path");
-});
+app.get("/search", (req, res) => {
+    let { q } = req.query;
+    console.log(req.query);
+    if(!q){
+        res.send("<h1>nothing searched</h1>");
+    }
+    res.send(`<h1>search result for query: ${q}</h1>`)
+})
 
-app.get('*', (req, res) => {
-    res.send("this path does not exits");
-});
+// app.get("/apple", (req, res) => {
+//     res.send("you contacted apple path");
+// });
 
-app.post("/", (req, res) => {
-    res.send("you send a post request to root");
-});
+// app.get("/orange", (req, res) => {
+//     res.send("you contacted orange path");
+// });
+
+// app.get("*", (req, res) => {
+//     res.send("you contacted orange path");
+// });
+
+// app.post("/", (req, res) => {
+//     res.send("you send a post request to root");
+// });
 
 // app.use((req, res) => {
 //     console.log("request received");
