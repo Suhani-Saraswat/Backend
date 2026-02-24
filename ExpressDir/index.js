c5aa46ccfc0c7e63dc1e8bf52de4d6e4f4753232
Suhani Saraswat <suhanisaraswat1795@gmail.com>
2026-02-24 21:29:23 +0530

    Get Response

--- ExpressDir/index.js
+++ ExpressDir/index.js
@@ -7,8 +7,28 @@ app.listen(port , () => {
     console.log(`App is listening on port ${port}`);
 });
 
-app.use((req, res) => {
-    console.log("request received");
-    let code = "<h1>Fruits</h1> <ul><li>Apple</li><li>Mango</li></ul>"
-    res.send(code);
+app.get("/", (req, res) => {
+    console.log("you contacted root path");
 });
+
+app.get("/apple", (req, res) => {
+    console.log("you contacted apple path");
+});
+
+app.get("/orange", (req, res) => {
+    console.log("you contacted orange path");
+});
+
+app.get("*", (req, res) => {
+    console.log("this path does not exits");
+});
+
+app.post("/", (req, res) => {
+    console.log("you send a post request");
+});
+
+// app.use((req, res) => {
+//     console.log("request received");
+//     let code = "<h1>Fruits</h1> <ul><li>Apple</li><li>Mango</li></ul>"
+//     res.send(code);
+// });
