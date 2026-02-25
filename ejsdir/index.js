35624f3157f684eaec673afd5bfb56426c2cb38a
Suhani Saraswat <suhanisaraswat1795@gmail.com>
2026-02-25 21:56:36 +0530

    Followers on insta

--- ejsdir/index.js
+++ ejsdir/index.js
@@ -12,8 +12,9 @@ app.get("/", (req, res) => {
 });
 
 app.get("/ig/:username", (req, res) => {
+    const followers = ["adam", "bob", "steve", "abc"];
     let { username } = req.params;
-    res.render("instagram.ejs", { username });
+    res.render("instagram.ejs", { username, followers });
 });
 
 app.get("/rolldice", (req, res) => {
