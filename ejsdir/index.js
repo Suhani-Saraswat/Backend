476264f20d9d0e4e5978f29c0040e04149c8949e
Suhani Saraswat <suhanisaraswat1795@gmail.com>
2026-02-25 21:41:35 +0530

    Instagram

--- ejsdir/index.js
+++ ejsdir/index.js
@@ -11,6 +11,11 @@ app.get("/", (req, res) => {
     res.render("home.ejs");
 });
 
+app.get("/ig/:username", (req, res) => {
+    let { username } = req.params;
+    res.render("instagram.ejs", { username });
+});
+
 app.get("/rolldice", (req, res) => {
     let diceVal = Math.floor(Math.random() * 6) + 1;
     res.render("rolldice.ejs", { diceVal });
