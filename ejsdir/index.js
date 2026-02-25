efa643bf02b1feed5cabedf3cf6798c400517be9
Suhani Saraswat <suhanisaraswat1795@gmail.com>
2026-02-25 20:47:01 +0530

    View template in ejs

--- ejsdir/index.js
+++ ejsdir/index.js
@@ -3,6 +3,12 @@ const app = express();
 
 let port = 8080;
 
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("home.ejs");
+});
+
 app.listen(port , () => {
     console.log(`App is listening on port ${port}`);
 });
