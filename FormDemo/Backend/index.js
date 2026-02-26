0319259866abf19efde836565122490f949b69b8
Suhani Saraswat <suhanisaraswat1795@gmail.com>
2026-02-26 20:16:53 +0530

    Parsing Data

--- FormDemo/Backend/index.js
+++ FormDemo/Backend/index.js
@@ -2,13 +2,17 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-app.get("/require", (req, res) => {
-    let { user, password } = res.query;
-    res.send(`submitted GET response.Welcome ${user}`);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/register", (req, res) => {
+    let { user, password } = req.query;
+    res.send(`submitted GET response. Welcome ${user}!`);
 });
 
-app.post("/require", (req, res) => {
-    res.send("submitted POST response");
+app.post("/register", (req, res) => {
+    let { user, password } = req.body;
+    res.send(`submitted POST response. Welcome ${user}!`);
 });
 
 app.listen(port, () => {
