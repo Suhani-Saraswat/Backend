d51fafe5ab6330e8a92eedb0e13cb25de5e954ab
Suhani Saraswat <suhanisaraswat1795@gmail.com>
2026-02-26 22:26:48 +0530

    Inheritance

--- FormDemo/Frontend/app.js
+++ FormDemo/Frontend/app.js
@@ -14,12 +14,52 @@
 
 
 //Constructors - doesn't return anything & start with capital
-function Person(name, age) {
-    this.name = name;
-    this.age = age;
+// function Person(name, age) {
+//     this.name = name;
+//     this.age = age;
+// }
+// Person.prototype.talk = function () {
+//     console.log(`Hi, my name is ${this.name}`);
+// };
+// let p1 = new Person("adam", 25);
+// let p2 = new Person("eve", 25);
+
+
+//Classes
+// class Person {
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+
+//     talk() {
+//         console.log(`Hi, my name is ${this.name}`);
+//     }
+// }
+// let p1 = new Person("adam", 25);
+// let p2 = new Person("eve", 25);
+
+//Inheritance
+class Person {
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+    talk() {
+        console.log(`Hi, my name is ${this.name}`);
+    }
 }
-Person.prototype.talk = function () {
-    console.log(`Hi, my name is ${this.name}`);
-};
-let p1 = new Person("adam", 25);
-let p2 = new Person("eve", 25);
+
+class Student extends Person {
+    constructor(name, age, marks) {
+        super(name, age); //parent class contructor is being called
+        this.marks = marks;
+    }
+}
+
+class Teacher extends Person {
+    constructor(name, age, subject) {
+        super(name, age); //parent class contructor is being called
+        this.subject = subject;
+    }
+}
