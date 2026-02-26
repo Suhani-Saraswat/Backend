42f8b3805b8b3b4a2b25737d46ddb8ad5b7965a6
Suhani Saraswat <suhanisaraswat1795@gmail.com>
2026-02-26 22:12:34 +0530

    Constructors

--- FormDemo/Frontend/app.js
+++ FormDemo/Frontend/app.js
@@ -15,13 +15,11 @@
 
 //Constructors - doesn't return anything & start with capital
 function Person(name, age) {
-    const person = {
-        name: name,
-        age: age,
-        talk() {
-            console.log(`Hi, my name is ${this.name}`);
-        },
-    };
-
-    return person;
+    this.name = name;
+    this.age = age;
 }
+Person.prototype.talk = function () {
+    console.log(`Hi, my name is ${this.name}`);
+};
+let p1 = new Person("adam", 25);
+let p2 = new Person("eve", 25);
