79a88e4cc46ab57f7a46bcdc59d3be22241cf252
Suhani Saraswat <suhanisaraswat1795@gmail.com>
2026-03-05 16:41:23 +0530

    Books.js

---
+++ mongo/books.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+main()
+    .then(() => {
+        console.log("connection successful");
+    })
+    .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/amazon');
+}
+
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: String,
+    },
+    price: {
+        type: Number,
+    },
+});
+
+const Book = mongoose.model("Book", bookSchema);
