82912418b9403230c713f71c120b5146956bb64e
Suhani Saraswat <suhanisaraswat1795@gmail.com>
2026-03-09 21:02:17 +0530

    Api token query

--- middlewares/app.js
+++ middlewares/app.js
@@ -24,6 +24,19 @@ app.use("/random", (req, res, next) => {
     next();
 });
 
+//API Access Token
+app.use("/api", (req, res, next) => {
+    let { token } = req.query;
+    if (token === "giveaccess") {
+        next();
+    }
+    res.send("ACCESS DENIED!");
+});
+
+app.get("/api", (req, res) => {
+    res.send("Data");
+});
+
 app.get("/", (req, res) => {
     res.send("Hi, I am root");
 });
